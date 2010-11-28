d8d184804e71633b8238adc77ea721d9947458bb
mlyczek <mlyczek@debian>
2010-11-28 16:14:27 +0100

    Initial upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projekt::Application.config.secret_token = 'c0eea7b6845c62bd8aafedb57b2078bd9fed218652a2fb59a7812e5618813461e3e4d3ecd477489b28f7856a617db70db96a725516daddcbb85162533535cd24'
