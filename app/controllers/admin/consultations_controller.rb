6ab31624ccde23c185615bee633db1bdd8bdb56e
Mateusz Łyczek <mlyczek@debian>
2010-12-14 23:43:24 +0100

    When user bounded to FB add consultation, it is also posted to his FB wall

--- app/controllers/admin/consultations_controller.rb
+++ app/controllers/admin/consultations_controller.rb
@@ -6,6 +6,7 @@ class Admin::ConsultationsController < Admin::AdminController
 
 	def create
 		@consult = Consultation.create(params[:consultation])
+		post_on_fb(@consult)
 
 		redirect_to admin_consultations_path
 	end
@@ -15,5 +16,22 @@ class Admin::ConsultationsController < Admin::AdminController
 		@consult.destroy()
 
 		redirect_to admin_consultations_path
+	end
+
+	private
+	def post_on_fb(cons)
+		if not current_teacher.profile.fb_access_token.nil?
+			require 'net/https'
+
+			url = URI.parse("https://graph.facebook.com/#{current_teacher.profile.fb_profile_id}/feed")
+			http = Net::HTTP.new(url.host, url.port)
+			http.use_ssl = (url.scheme == 'https')
+			request = Net::HTTP::Post.new(url.path)
+			request.set_form_data({'access_token' => current_teacher.profile.fb_access_token,
+														'message' => "Dodałem konsultacje w dniu: "+cons.day}, '&')
+			response = http.request(request)
+
+			flash[:notice] = response.body
+		end
 	end
 end
