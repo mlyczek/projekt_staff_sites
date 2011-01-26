91c2cf11cce9878b54ed90db8c811b5ab95fece2
Mateusz Łyczek <mlyczek@debian>
2011-01-26 12:15:11 +0100

    Editing and showing lecture

--- app/controllers/admin/lectures_controller.rb
+++ app/controllers/admin/lectures_controller.rb
@@ -19,4 +19,33 @@ class Admin::LecturesController < Admin::AdminController
 			render :action => "new"
 		end
 	end
+
+	def edit
+		@subject = get_current_subject(params[:subject_id])
+		@lecture = @subject.lectures.find(params[:id])
+	end
+
+	def update
+		@subject = get_current_subject(params[:subject_id])
+		@lecture = @subject.lectures.find(params[:id])
+
+		if @lecture.update_attributes(params[:lecture])
+			redirect_to admin_subject_lectures_path(@subject)
+		else
+			render :action => "edit"
+		end
+	end
+
+	def show
+		@subject = get_current_subject(params[:subject_id])
+		@lecture = @subject.lectures.find(params[:id])
+	end
+
+	def destroy
+		@subject = get_current_subject(params[:subject_id])
+		@lecture = @subject.lectures.find(params[:id])
+
+		@lecture.destroy
+		redirect_to admin_subject_lectures_path(@subject)
+	end
 end
