40edf99672a46db8c27e033d07f43389703fd9d5
Mateusz Łyczek <mlyczek@debian>
2011-02-04 14:19:24 +0100

    Teacher can now edit publications

--- app/controllers/admin/publications_controller.rb
+++ app/controllers/admin/publications_controller.rb
@@ -10,6 +10,20 @@ class Admin::PublicationsController < Admin::AdminController
 		redirect_to admin_publications_path
 	end
 
+	def edit
+		@pub = current_teacher.publications.find(params[:id])
+	end
+
+	def update
+		@pub = current_teacher.publications.find(params[:id])
+
+		if @pub.update_attributes(params[:publication])
+			redirect_to admin_publications_path
+		else
+			render :action => "edit"
+		end
+	end
+
 	def destroy
 		pub = current_teacher.publications.find(params[:id])
 		pub.destroy()
