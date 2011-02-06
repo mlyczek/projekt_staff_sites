a50f13f7fe788d670faa82f81360ff09fb5c42b2
Mateusz Łyczek <mlyczek@debian>
2011-02-07 00:31:53 +0100

    Timetable is now successfully showing.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,35 @@
 class Event < ActiveRecord::Base
   belongs_to :timetable
 	belongs_to :event_type
+
+	# podaje ile minut trwa dane wydarzenie/zajęcia
+	def duration_in_minutes
+		return ((self.end - self.start) / (60 * 2)).to_i;
+	end
+
+	# podaje ile minut upłynęło od początku dnia do rozpoczęcia wydarzenia
+	def minutes_since_beginning
+		beginning = Time.utc(start.year, start.month, start.day, 7, 0)
+
+		return ((start - beginning) / (60 * 2)).to_i
+	end
+
+	# podaje ile pixeli powinna wynosić wysokość wydarzenia (uwzględnia też linie poziome, które są szerokości 1px)
+	def height_pixels
+		duration = duration_in_minutes
+
+		if duration > 30
+			duration = duration + (duration / 30).to_i
+		end
+
+		return duration.to_s + "px"
+	end
+
+	# podaje ile pixeli od góry wydarzenie powinno być przesunięte (uwzględnia też linie poziome, które są szerokości 1px)
+	def pixels_from_top
+		minutes = minutes_since_beginning
+		pixels = (minutes + (minutes / 30) -1).to_i
+
+		return pixels.to_s + "px"
+	end
 end
