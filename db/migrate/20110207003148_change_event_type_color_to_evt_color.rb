4cbbd3d8a2aaa735ab260aca8cddc48aaff1323b
Mateusz Łyczek <mlyczek@debian>
2011-02-07 12:08:56 +0100

    Another fix to heroku/Postgress casting problem

--- db/migrate/20110207003148_change_event_type_color_to_evt_color.rb
+++ db/migrate/20110207003148_change_event_type_color_to_evt_color.rb
@@ -1,21 +1,21 @@
 class ChangeEventTypeColorToEvtColor < ActiveRecord::Migration
   def self.up
+		remove_column :event_types, :color
+		add_column :event_types, :event_type_color_id, :integer
+
 		EventType.all.map { |et|
 			if et.name == "Zajęcia w ii"
-				et.update_attributes(:color => "1")
+				et.update_attributes(:event_type_color_id => 1)
 			else
 				if et.name == "Zajęcia poza ii"
-					et.update_attributes(:color => "2")
+					et.update_attributes(:event_type_color_id => 2)
 				end
 			end
 		}
-
-		change_column :event_types, :color, :integer
-		rename_column :event_types, :color, :event_type_color_id
   end
 
   def self.down
-		rename_column :event_types, :event_type_color_id, :color
-		change_column :event_types, :color, :string
+		remove_column :event_types, :event_type_color_id
+		add_column :event_types, :color, :string
   end
 end
