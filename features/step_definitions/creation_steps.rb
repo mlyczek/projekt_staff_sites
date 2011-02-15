8f3c90d29e28fea6b9483c554894b343733ae26d
Mateusz Łyczek <mlyczek@debian>
2011-02-15 18:35:21 +0100

    Added creation steps for publications

--- features/step_definitions/creation_steps.rb
+++ features/step_definitions/creation_steps.rb
@@ -2,6 +2,8 @@
 # File with steps that are creating some data to use in scenario
 #
 
+# ----- consultations --------
+
 Given /^the following consultations:$/ do |consultations|
 	Consultation.create!(consultations.hashes)
 	Then %{there should be #{consultations.hashes.length} consultations}
@@ -12,5 +14,20 @@ Then /^there should be (.*) consultations$/ do |n|
     Consultation.all.count.should == n.to_i
   else
     assert_equal n.to_i, Consultation.all.count
+  end
+end
+
+# ----- publications --------
+
+Given /^the following publications:$/ do |publications|
+	Publication.create!(publications.hashes)
+	Then %{there should be #{publications.hashes.length} publications}
+end
+
+Then /^there should be (.*) publications$/ do |n|
+	if defined?(Spec::Rails::Matchers)
+    Publication.all.count.should == n.to_i
+  else
+    assert_equal n.to_i, Publication.all.count
   end
 end
